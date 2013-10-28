fb9517c252dab2629bdca19151637859a1c5fda7
Greg Schafer <grschafer@gmail.com>
2013-10-28 16:44:35 -0500

    Move env initialization from zshrc to zprofile
    Also add pbcopy alias and add vim as default editor

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -5,3 +5,7 @@ alias ls="ls -F --color"
 alias l="ls -lAh --color"
 alias ll="ls -al --color"
 alias la='ls -A --color'
+
+# http://whereswalden.com/2009/10/23/pbcopy-and-pbpaste-for-linux/
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
